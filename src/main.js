466a75728963e5e77aba728b66671b2c054899b7
12345 <13512091051@163.com>
2019-06-29 19:34:02 +0800

    删除功能完成

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import './styles/index.less'
 import 'nprogress/nprogress.css'
 import axios from 'axios'
 import { getUser,removeUser } from '@/utils/auth'
+import JSONbig from 'json-bigint'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 //  config 是本次请求相关的配置对象
 // axios 请求拦截器 发出的请求会先经过这里
@@ -22,8 +23,28 @@ axios.interceptors.request.use( (config)=> {
   // Do something with request error
   return Promise.reject(error);
 })
-
-
+//  axios 预留的自定义处理后端返回的数据
+//  也可以理解成是一个响应拦截器 这里比较特殊 
+//  data是后端返回的未经处理的原始数据
+//  axios 默认使用JSON.parse去转换原始数据  如果其中出现超出范围的数据就会出现问题
+//  所以我们也可以手动处理这些原始数据
+//    npm i json-bigint
+//    JSONbig.parse(原始数据JSON字符串)
+axios.defaults.transformResponse = [function (data) {
+  // Do whatever you want to transform the data
+  // console.log(data)
+  // return data;
+  // 使用 JSONbig.parse 转换原始数据  类似于 JSON.parse  但是他会处理超出安全范围的整数问题
+  // 严谨一点如果data不是JSON格式字符串就会报错
+  try {
+    //  如果是json格式字符串就转换并返回留给后续使用
+  return JSONbig.parse(data)
+  }catch (err) {
+   //  如果不是JSON 格式字符串就直接鸳鸯返回给后续使用
+    console.log(err)
+    return data
+  }
+}],
 
 
 // axios 回应拦截器 接受的响应先来这
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -5,10 +5,10 @@
         <span>数据筛选</span>
         <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
       </div>
-       <el-form ref="form" :model="filterParams" label-width="80px">
+      <el-form ref="form" :model="filterParams" label-width="80px">
         <el-form-item label="状态">
           <el-radio-group v-model="filterParams.status">
-            <el-radio label="">全部</el-radio>
+            <el-radio label>全部</el-radio>
             <el-radio
               v-for="(item, index) in statusType"
               :key="item.label"
@@ -17,32 +17,23 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道">
-            <el-select v-model= "filterParams.channel_id" clearable>
-               <el-option 
-               v-for = "item in channels"
-               :label = "item.name"
-               :key = "item.id"
-               :value = "item.id"
-               ></el-option>
-            </el-select>
+          <el-select v-model="filterParams.channel_id" clearable>
+            <el-option v-for="item in channels" :label="item.name" :key="item.id" :value="item.id"></el-option>
+          </el-select>
         </el-form-item>
-       <el-form-item label="时间">
+        <el-form-item label="时间">
           <el-date-picker
-            @click = "handleDateChange"
-            value-firmat = 'yyyy-MM-dd'
-            v-model = "range_date"
+            @click="handleDateChange"
+            value-firmat="yyyy-MM-dd"
+            v-model="range_date"
             type="daterange"
             range-separator="至"
             start-placeholder="开始日期"
-            end-placeholder="结束日期">
-          </el-date-picker>
+            end-placeholder="结束日期"
+          ></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button
-            type="primary"
-            :loading="articleLoading"
-            @click = "handleFilter"
-          >查询</el-button> 
+          <el-button type="primary" :loading="articleLoading" @click="handleFilter">查询</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -58,7 +49,7 @@
         当前遍历对象就像访问item一样
     -->
 
-    <el-table class="article-list" :data= "articles" style="width: 100%" v-loading= "articleLoading">
+    <el-table class="article-list" :data="articles" style="width: 100%" v-loading="articleLoading">
       <el-table-column label="封面" width="180">
         <template slot-scope="scope">
           <img v-for="item in scope.row.cover.images" :src="item" :key="item">
@@ -74,7 +65,7 @@
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button size="mini" type="primary" plain>修改</el-button>
-          <el-button size="mini" type="danger" plain  @click= "handleDelete(scope.row)">删除</el-button>
+          <el-button size="mini" type="danger" plain @click="handleDelete(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -86,10 +77,10 @@
     <el-pagination
       background
       layout="prev, pager, next"
-      :page-size= "pageSize"
-      :total= "totalCount"
-      @current-change= "handleCurrentChange"
-      :disabled= "articleLoading"
+      :page-size="pageSize"
+      :total="totalCount"
+      @current-change="handleCurrentChange"
+      :disabled="articleLoading"
     ></el-pagination>
   </div>
 </template>
@@ -129,58 +120,97 @@ export default {
       articleLoading: false, //  加载中
       filterParams: {
         //   要返回的数据
-        status: '',  //  文章状态
-        channel_id: '', //  频道id
-        begin_pubdate: '',//  开始时间
-        end_pubdate: ''//  结束时间
-        },
-       channels:[],
-       range_date: ''  //  事件范围绑定 这个字段在这里无意义主要是为了触发handleDateChange事件
-    }
+        status: "", //  文章状态
+        channel_id: "", //  频道id
+        begin_pubdate: "", //  开始时间
+        end_pubdate: "" //  结束时间
+      },
+      channels: [],
+      range_date: " " //  事件范围绑定 这个字段在这里无意义主要是为了触发handleDateChange事件
+    };
   },
   created() {
-    this.loadAticles(),
-    this.loadChannels()
+    this.loadAticles(), this.loadChannels();
   },
   methods: {
-   handleDelete(item){
-     console.log(item)
-   },
+      async handleDelete (item) {
+      try {
+        // 删除确认提示
+        // 如果手动catch了异常还是会被外部的try catch捕获到
+        //  但是代码可以继续往后执行
+        await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+        // 如果手动 catch 了它的异常，还是会被外部的 try-catch 捕获到
+        // 但是代码依然可以继续往后执行
+        // .catch(() => {
+        //   this.$message({
+        //     type: 'info',
+        //     message: '已取消删除'
+        //   })
+        // })
+
+        // 确认：执行删除操作
+        await this.$http({
+          method: 'DELETE',
+          url: `/articles/${item.id}`
+        })
+
+        this.$message({
+          type: 'success',
+          message: '删除成功'
+        })
+
+        // 删除成功重新加载数据列表
+        this.loadArticles()
+      } 
+      catch (err) {
+        if (err === 'cancel') {
+          return this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        }
+        this.$message.error('删除失败')
+      }
+    },
     //  点击查询按钮 根据文章列表加载文章
-   handleFilter () {
-     this.page = 1
-     this.loadAticles()
-   },
-   handleDateChange(value){
-     this.filteerParams.begin_pubdate = value[0],
-     this.filterParams.end_pubdate = value[1]
-     //  输出数组把第0项给begin  第二项给 end
-   },
-   async loadChannels () {
-   try {
-      const data = await this.$http({
-       method: 'GET',
-       url: '/channels'
-     })  
-    //  console.log(data)
-     this.channels = data.channels
-   } catch (err) {
-      this.$message.error('获取频道失败')
-   } 
-  },
+    handleFilter() {
+      this.page = 1;
+      this.loadAticles()
+    },
+    handleDateChange(value) {
+        this.filteerParams.begin_pubdate = value[0]
+        this.filterParams.end_pubdate = value[1]
+      //  输出数组把第0项给begin  第二项给 end
+    },
+    async loadChannels() {
+      try {
+        const data = await this.$http({
+          method: "GET",
+          url: "/channels"
+        });
+        //  console.log(data)
+        this.channels = data.channels;
+      } catch (err) {
+        this.$message.error("获取频道失败")
+      }
+    },
     //  请求开始
     //  请求开始加载loading
-    async loadAticles() {
-      this.articleLoading = true
+    async loadAticles(){
+     try{
+       this.articleLoading = true
       //  除了登陆相关接口之外其他的接口都必须在请求头中通过Authorization字段提供用户token
       //  当我们登陆成功服务端会生成一个token令牌放到用户信息中
 
-
       //  在加载文章中筛选无用数据
       const filterData = {}
-      for(let key in this.filterParams){
+      for (let key in this.filterParams) {
         const item = this.filterParams[key]
-        if(item !=='' && item !== null && item !== undefined){
+        if (item !== "" && item !== null && item !== undefined) {
           filterDta[key] = item
         }
       }
@@ -200,21 +230,25 @@ export default {
           status: filterData.status
           //  ...filterData     将filterData混入当前对象中
         }
-      });
-      this.articles = data.results;
-      this.totalCount = data.total_count;
+      })
+      this.articles = data.results
+      this.totalCount = data.total_count
       //  请求结束  停止loading
       this.articleLoading = false
+     } catch(err) {
+       this.$message.error("加载文章列表失败")
+     }
     },
+    
     //  组件说明  参数为当前页
     handleCurrentChange(page) {
       //  将数据中的页码修改为当前改变的页码
       this.page = page;
       //  改变页码之后再进行文章的重新加载
-      this.loadAticles();
+      this.loadAticles()
     }
   }
-};
+}
 </script>
 
 <style lang="less" scoped>
