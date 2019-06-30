ed5eb300c41d3f76686fc6daf8a6dc4f7f921300
12345 <13512091051@163.com>
2019-06-30 14:14:07 +0800

    发布简单版完成

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,17 +1,81 @@
 <template>
-    <div>
-        <p>Publish Component</p>
+  <el-card class="publish-card">
+    <div slot="header" class="header">
+      <span>发布文章</span>
+      <div>
+        <el-button type="success" @click="handlePublish(false)">发布</el-button>
+        <el-button type="primary" @click="handlePublish(true)">存入草稿</el-button>
+      </div>
     </div>
+    <!-- 表单 -->
+    <el-form ref="form" :model="articleForm" label-width="80px">
+      <el-form-item label="标题">
+        <el-input v-model="articleForm.title"></el-input>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input type="textarea" v-model="articleForm.content"></el-input>
+      </el-form-item>
+      <el-form-item label="封面"></el-form-item>
+      <el-form-item label="频道">
+        <el-select v-model="articleForm.channel_id" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <!-- /表单 -->
+  </el-card>
 </template>
 <script>
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+import { quillEditor } from 'vue-quill-editor'
 export default {
-    name:'AppPublish',
-    data () {
-        return {
-
+  name: "AppPublish",
+  data() {
+    return {
+      articleForm: {
+        title: "",
+        content: "",
+        channel_id: "3",
+        cover: {
+          type: 0,
+          images: []
         }
+      }
     }
+  },
+  methods: {
+  
+    async handlePublish(draft) {
+      try {
+       await this.$http({
+          method: "POST",
+          url: "/articles",
+          params: {
+            draft
+          },
+          data: this.articleForm
+        })
+        this.$message({
+          type: "success",
+          message: "发布成功"
+        })
+      } catch (err) {
+        this.$message.error("发布失败", err)
+      }
+    }
+  }
 }
 </script>
 <style lang='less' scoped>
+.publish-card {
+  .header {
+    min-height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
 </style>
